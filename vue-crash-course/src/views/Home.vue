461591ccb71fa84411e71a24999f7d2dced42554
Damirbek <busurmankulov577@gmail.com>
2021-04-05 14:26:35 +0600

    Vue

---
+++ vue-crash-course/src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h2>Home Page</h2>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit omnis obcaecati quia tenetur, maiores vel!</p>
+        <a href="/todos">Todos</a>
+    </div>
+</template>
